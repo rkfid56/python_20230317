9202da38d5ccc90240492f3373c33d33edb8bd4a
rkfid56 <128141111+rkfid56@users.noreply.github.com>
2023-03-17 19:36:42 +0900

    수업자료 첫 업로드

---
+++ src/com/python/study/classStudy/CarMain.py
@@ -0,0 +1,22 @@
+from src.com.python.study.classStudy.Car import Car
+
+# if __name__ == '__main__':
+#     c1 = Car('현대자동차', '펠리세이드', '화이트')
+#     c1.carInfo()
+#     c2 = Car('현대자동차', '쏘나타', '블랙')
+#     c2.carInfo()
+
+
+if __name__ == '__main__':
+    carList = list()
+
+    c1 = Car('현대자동차', '펠리세이드', '화이트')
+    c2 = Car('현대자동차', '쏘나타', '블랙')
+
+    # print(c1)
+    carList.append(c1)
+    carList.append(c2)
+
+    for carObj in carList:
+        # print(carObj)
+        print(f'회사명: {carObj.model}')
---
+++ src/com/python/study/classStudy/StudentMain.py
@@ -0,0 +1,11 @@
+from src.com.python.study.classStudy.Student import Student
+
+# if __name__ == '__main__':
+#     student = Student('김준일', 30)
+#     student2 = Student('김준이', 31)
+#     student.showInfo()
+#     student2.showInfo()
+#     Student.classMethod('김현욱')
+#     Student.classMethod2()
+#     student.classMethod('이종현')
+#     Student.classMethod2()

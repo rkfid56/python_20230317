5295831f4efe3f0cbee879cd22c27c92584513ad
rkfid56 <128141111+rkfid56@users.noreply.github.com>
2023-03-23 21:51:56 +0900

    2023.03.23 커밋
    python>Authontication,service

--- src/com/python/Authontication/main/Main.py
+++ src/com/python/Authontication/main/Main.py
@@ -1,7 +1,19 @@
 if __name__ == "__main__":
+    while True:
+        print("=====<< Python Auth>>=====")
+        print("1. 로그인")
+        print("2. 회원가입")
+        print("q. 프로그램 종료")
+        print("==========================")
+        select = input("Menu Selected!! >>> ")
 
-    print("=====<< Python Auth>>=====")
-    print("1. 로그인")
-    print("2. 회원가입")
-    print("q. 프로그램 종료")
-    print("==========================")
+        if select == 'q':
+            break;
+        elif select == '1':
+            pass
+        elif select == '2':
+            pass
+        else:
+            print("다시 입력하세요.")
+        print("==========================")
+        print()
